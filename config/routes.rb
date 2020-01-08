b579157b63e7e1c7cbd10eb24791e3454dbc81dd
Keith Isham <kisham8899@gmail.com>
2020-01-08 11:27:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  # resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs
+  resources :genres
+  resources :artists
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
