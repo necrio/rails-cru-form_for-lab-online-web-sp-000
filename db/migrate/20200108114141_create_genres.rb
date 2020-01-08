368691c4b8cd30725deaf13e5f2344e7948ab775
Keith Isham <kisham8899@gmail.com>
2020-01-08 11:42:24 +0000

    Automatically backed up by Learn

--- db/migrate/20200108114141_create_genres.rb
+++ db/migrate/20200108114141_create_genres.rb
@@ -1,6 +1,9 @@
-class CreateGenres < ActiveRecord::Migration[5.0]
+class CreateGenres < ActiveRecord::Migration
   def change
     create_table :genres do |t|
+      t.string :name
+
+      t.timestamps null: false
     end
   end
 end
