8ade461611f00556c84ff791dcd08937c46b9892
Keith Isham <kisham8899@gmail.com>
2020-01-08 11:42:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200108114159_create_songs.rb
+++ db/migrate/20200108114159_create_songs.rb
@@ -1,6 +1,12 @@
-class CreateSongs < ActiveRecord::Migration[5.0]
-  def change
-    create_table :songs do |t|
-    end
+
+class CreateSongs < ActiveRecord::Migration
+def change
+  create_table :songs do |t|
+    t.string :name
+    t.integer :artist_id
+    t.integer :genre_id
+
+    t.timestamps null: false
   end
+end
 end
